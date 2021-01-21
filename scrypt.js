15e20b4d1d4f9d1f070d6e503a13f3d524b7847f
Raulosino <eduarteraul@gmail.com>
2021-01-21 12:36:36 +0100

    version 1

---
+++ scrypt.js
@@ -0,0 +1,14 @@
+
+class Person {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+        this.office = null;
+        this.company = null;
+        this.building = null;
+    }
+}
+
+
+
+
